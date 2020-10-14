940ba9db2aca95cfb229d7239113fe4042c7734b
Jacob Reid <72713602+JacobReidWD@users.noreply.github.com>
2020-10-14 03:06:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-var className = document.getElementByClassName("dropdown-content");
-
-document.className.style.display = "none";
-document.className.style.position = "absolute";
-document.className.style.backgroundColor = "#f1f1f1";
-document.className.style.minWidth = "160px";
-document.className.style.boxShadow = "0px 8px 16px 0px rgba(0, 0, 0, 0.2)";
-document.className.style.zIndex = "1";
-
 function dropDown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
