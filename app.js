9a4dfe9c5f07ddba954a9b9ffcd129ca3788eb70
Jacob Reid <72713602+JacobReidWD@users.noreply.github.com>
2020-10-14 00:46:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-function openTab(evt, tabLink) {
-  var i, tabcontent, tablinks;
-
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  document.getElementById(tabLink).style.display = "block";
-  evt.currentTarget.className += " active";
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
