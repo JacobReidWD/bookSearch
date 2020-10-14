f5c721b7fae037e76145be247889c73feaf22c05
Jacob Reid <72713602+JacobReidWD@users.noreply.github.com>
2020-10-13 23:39:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var dict = {
-  : "Chris",
-  "one": 1,
-  1: "some value"
-};
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
