fcb063c6a45d5336929b26648382349ac4d66d3b
Jacob Reid <72713602+JacobReidWD@users.noreply.github.com>
2020-10-14 22:50:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+function show(id) {
+  document.getElementById(id).style.display = "block";
+}
+function noShow(id) {
+  document.getElementById(id).style.display = "none";
+}
 function dropDown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-  
+document.getElementById("myDropdown").style.color = "red";
+// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
